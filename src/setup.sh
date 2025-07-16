ca60a8d40aff8803c510d02844942231dde2bf89
ponponmama <buti1024@gmail.com>
2025-07-16 22:29:50 +0900

    README.md修正

---
+++ src/setup.sh
@@ -0,0 +1,18 @@
+mkdir -p bootstrap/cache \
+        storage \
+        storage/framework/cache \
+        storage/framework/cache/data \
+        storage/framework/sessions \
+        storage/framework/testing \
+        storage/framework/views \
+        storage/logs \
+        storage/logs/app \
+        storage/logs/app/public
+
+# コンテナ内で実行されるため、sudoは不要
+chown -R www-data:www-data storage
+
+# 必要な権限を設定
+chmod -R 775 storage
+
+echo "ディレクトリが正常に作成され、必要な権限が設定されました。"
