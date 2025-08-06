e00e4a243aec6f087419451abbbe9f26467d345c
ponponmama <buti1024@gmail.com>
2025-08-06 15:35:01 +0900

    setup.sh修正

--- src/setup.sh
+++ src/setup.sh
@@ -9,11 +9,14 @@ mkdir -p bootstrap/cache \
         storage/logs/app \
         storage/logs/app/public
 
+echo "ディレクトリ作成完了"
+
 # コンテナ内で実行されるため、sudoは不要
 chown -R www-data:www-data storage
+echo "chown実行完了"
 
 # 必要な権限を設定
 chmod -R 775 storage
-
+echo "chmod実行完了"
 
 echo "ディレクトリが正常に作成され、必要な権限が設定されました。"
